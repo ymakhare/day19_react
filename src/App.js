a087c1812024587379dbe0b1b1ab248111613922
ymakhare <yog.makhare@gmail.com>
2022-01-03 16:45:55 +0530

    style Updation in B2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 /**
- * Class - in JS this is reserved keyword
- * because of that we have to use "classname" instead of "class" in JS..
- * @returns
+ * style = ""
+ * style="background-color:red"
+ *
+ * RULE
+ * style = {{}}
+ * style = {{backgroundColor: "red"}}
+ *
+ * 1. First Two Letter Css Property are now Converted into camelCase proprty.
+ * 2. The value of the property must be in "DOUBLE QOUTE"
  */
-
 export default function App() {
   return (
     <div>
-      <h1 className="alert-success p-4 text-black">Hello boys...</h1>;
-      <h1 className="alert-warning p-4 text-danger">how are you????</h1>
+      <div>
+        <h1
+          style={{
+            height: "50px",
+            backgroundColor: "Green",
+            fontSize: "30px",
+            padding: "8px",
+            textAlign: "center",
+            color: "white"
+          }}
+        >
+          Hello Vrushabh and Vishvadip
+        </h1>
+      </div>
     </div>
   );
 }
