00ef0fdf88bd30e681c668718d449c3d14792dbe
ymakhare <yog.makhare@gmail.com>
2022-01-04 22:35:39 +0530

    Twitter clone

--- src/App.js
+++ src/App.js
@@ -9,21 +9,65 @@ export default function App() {
 }
 
 function MyComponent() {
+  const [message, setMessaage] = useState("");
+  const [list, setList] = useState([]);
+
+  const tweet = (e) => {
+    const newMessage = e.target.value;
+    setMessaage(newMessage);
+  };
+
+  const post = () => {
+    const newList = [message, ...list];
+    setList(newList);
+    setMessaage("");
+    console.log(message);
+  };
+
+  const deletePost = () => {
+    list.splice(0, 1);
+    setList([...list]);
+  };
+
   let [counter, setCounter] = useState(0);
-  // function increment() {
-  //   setCounter(counter++);
-  // }
+  let [decounter, setdeCounter] = useState(0);
+
+  const increment = () => {
+    counter++;
+    setCounter(counter);
+  };
+
+  const decrement = () => {
+    decounter++;
+    setdeCounter(decounter);
+  };
+
   return (
     <div>
+      <div className="bg-dark text-light p-3 fs-1">My tweet App</div>
       <input
-        type="button"
-        value="Like me.."
-        onClick={() => setCounter(counter++)}
+        type="text"
+        placeholder="Tweet Here..."
+        value={message}
+        onChange={tweet}
       />
-      <div>Like {counter}</div>
-      <div>Like {counter}</div>
-      <div>Like {counter}</div>
-      <div>Like {counter}</div>
+      <input type="button" className="mx-2" value="Tweet" onClick={post} />
+      <input type="button" value="Delete Tweet" onClick={deletePost} />
+
+      {list.map((item) => (
+        <div>
+          <div className="bg-dark text-light mb-1"> {item} </div>
+          <input
+            type="button"
+            className="mb-1"
+            value="Like"
+            onClick={increment}
+          />
+          <span className="mx-2">{counter}</span>
+          <input type="button" value="Dislike" onClick={decrement} />
+          <span className="mx-2">{decounter}</span>
+        </div>
+      ))}
     </div>
   );
 }
