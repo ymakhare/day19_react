6f4c9a81abb26902c930545d72a67c30559a881c
ymakhare <yog.makhare@gmail.com>
2022-01-04 16:59:17 +0530

    Theme practice

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 export default function App() {
   return (
     <>
@@ -8,13 +9,28 @@ export default function App() {
 
 function MyComponent() {
   const id1 = 100;
-  let obj1 = { id: 1, username: "Vishakha" };
-  const background = "bg-danger";
-  return(
-    <div>
-      <h1>Interpolation {obj1.id + 100} </h1>
-      <h1 id={id1} className={background}>Interpolation {id1}</h1>
-    </div>
-  )
+  const list = [1, 1, 1, 1, 1, 1, 1, 1, 1];
+  const [defaultcss, setdefault] = useState("bg-info my-2 p-0 text-light");
 
+  const setRed = () => setdefault("bg-danger my-2 p-0 text-light");
+  const setGreen = () => setdefault("bg-success my-2 p-0 text-light");
+  const setDark = () => setdefault("bg-dark my-2 p-0 text-light");
+
+  return (
+    <div>
+      <input type="button" value="Red" onClick={setRed} />
+      <input type="button" value="Green" onClick={setGreen} />
+      <input type="button" value="Dark" onClick={setDark} />
+      {list.map(() => (
+        <div className={defaultcss}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+          necessitatibus est libero corrupti quidem! Ut libero perspiciatis
+          dignissimos aut, alias, ipsam nesciunt unde, iure eaque numquam
+          laborum optio qui eius veritatis quibusdam illo dolorum tempore itaque
+          ullam at velit? Tempore ad eligendi voluptates perferendis, deserunt
+          ea natus possimus doloribus soluta.
+        </div>
+      ))}
+    </div>
+  );
 }
