2b253d8b98845d5a6881f8ee8800b8b7e980a0b8
ymakhare <yog.makhare@gmail.com>
2022-01-04 15:04:40 +0530

    Backgrond attributes

--- src/App.js
+++ src/App.js
@@ -7,28 +7,14 @@ export default function App() {
 }
 
 function MyComponent() {
-  let name = "vrushabh";
-  let email = "vishv@dip";
-  let list = ["pune", "mumbai", "delhi"];
+  const id1 = 100;
   let obj1 = { id: 1, username: "Vishakha" };
-
-  return (
+  const background = "bg-danger";
+  return(
     <div>
-      <h1 className="bg-danger p-2">Learning Interpolation</h1>;
-      <h1>
-        {name} {email}
-      </h1>
-      <div> {list} </div>
-      <div> {list[1]} </div>
-      <div>{`My original ${obj1.id} and user name is ${obj1.username}. `}</div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
-        consequatur, ipsam itaque a facere dicta omnis dolores cupiditate
-        ratione voluptatem vitae quisquam iusto mollitia praesentium illo ullam
-        ipsa accusamus consequuntur neque? Nulla minima eveniet, quas sequi
-        magni, quibusdam ex aspernatur sapiente ut accusamus dolore! Vero rem
-        minus dolorum sequi earum.
-      </p>
+      <h1>Interpolation {obj1.id + 100} </h1>
+      <h1 id={id1} className={background}>Interpolation {id1}</h1>
     </div>
-  );
+  )
+
 }
