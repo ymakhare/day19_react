c956349537b05929e08546208499a106cf66377c
ymakhare <yog.makhare@gmail.com>
2022-01-04 20:31:24 +0530

    Counter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+
 export default function App() {
   return (
     <>
@@ -8,29 +9,21 @@ export default function App() {
 }
 
 function MyComponent() {
-  const id1 = 100;
-  const list = [1, 1, 1, 1, 1, 1, 1, 1, 1];
-  const [defaultcss, setdefault] = useState("bg-info my-2 p-0 text-light");
-
-  const setRed = () => setdefault("bg-danger my-2 p-0 text-light");
-  const setGreen = () => setdefault("bg-success my-2 p-0 text-light");
-  const setDark = () => setdefault("bg-dark my-2 p-0 text-light");
-
+  let [counter, setCounter] = useState(0);
+  // function increment() {
+  //   setCounter(counter++);
+  // }
   return (
     <div>
-      <input type="button" value="Red" onClick={setRed} />
-      <input type="button" value="Green" onClick={setGreen} />
-      <input type="button" value="Dark" onClick={setDark} />
-      {list.map(() => (
-        <div className={defaultcss}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
-          necessitatibus est libero corrupti quidem! Ut libero perspiciatis
-          dignissimos aut, alias, ipsam nesciunt unde, iure eaque numquam
-          laborum optio qui eius veritatis quibusdam illo dolorum tempore itaque
-          ullam at velit? Tempore ad eligendi voluptates perferendis, deserunt
-          ea natus possimus doloribus soluta.
-        </div>
-      ))}
+      <input
+        type="button"
+        value="Like me.."
+        onClick={() => setCounter(counter++)}
+      />
+      <div>Like {counter}</div>
+      <div>Like {counter}</div>
+      <div>Like {counter}</div>
+      <div>Like {counter}</div>
     </div>
   );
 }
